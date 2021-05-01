83a438b22b3760c90c27f9979c49b0e899678fc5
Zachary Wang <zlwang@haunter.cs.uchicago.edu>
2021-04-30 21:44:02 -0500

    slight edit to actions

--- scripts/action.py
+++ scripts/action.py
@@ -3,6 +3,7 @@
 import rospy
 from q_learning_project.msg import RobotMoveDBToBlock
 import numpy as np
+import moveit_commander
 
 '''
 This node will subscribe to robot_action and listen for commands to pick up or 
@@ -10,26 +11,25 @@ drop a dumbbell at a specific location. It will publish messages to cmd_vel
 and moveit to move turtlebot and arm according.
 '''
 
-class ActionNode(object):
+class ActioRobotnNode(object):
     def __init__(self):
         # Set up traffic status publisher
-        self.traffic_status_pub = rospy.Publisher("/traffic_status", Traffic)
-
-        # Counter to loop publishing direction with
-        self.direction_counter = 0
+        self.action_sub = rospy.Subscriber("/q_learning/robot_action", RobotMoveDBToBlock, self.signal_received)
+        self.movement_status_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        self.move_group_arm = moveit_commander.MoveGroupCommander("arm")
+        self.move_group_gripper = moveit_commander.MoveGroupCommander("gripper")
         rospy.sleep(1)
+
+    def signal_received(self, data):
+        
 
     def run(self):
         # Once every 10 seconds
         rate = rospy.Rate(0.1)
         while (not rospy.is_shutdown()):
-            trafficMsg = Traffic()
-            trafficMsg.direction = self.direction_counter % 3
-            self.direction_counter += 1
             self.traffic_status_pub.publish(trafficMsg)
             rate.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node('movement_controller')
-    action_node = ActionNode()
-    action_node.run()
+    robot = Robot()
+    robot.run()
