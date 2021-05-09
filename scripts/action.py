1f76aff0b9a6c78608cd74ca04952ae509b513c9
Jonah Arthur Kaye <jonahkaye@machoke.cs.uchicago.edu>
2021-05-09 18:29:36 -0500

    working rn on image rec

--- scripts/action.py
+++ scripts/action.py
@@ -7,6 +7,7 @@ import moveit_commander
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
+import keras_ocr
 
 
 '''
@@ -31,8 +32,8 @@ class ActionRobotNode(object):
         self.move_group_arm = moveit_commander.MoveGroupCommander("arm")
         self.move_group_gripper = moveit_commander.MoveGroupCommander("gripper")
         self.rest_pos = [0, .7, -.3, -.3]
-        self.lift_pos = [0, .4, -.6, -.15]
-        self.open_grip = [0.01, 0.01]
+        self.lift_pos = [0, .6, -.6, -.15]
+        self.open_grip = [0.012, 0.012]
         self.close_grip = [0.005, 0.005]
         self.move_group_arm.go(self.rest_pos, wait=True)
         self.move_group_gripper.go(self.open_grip, wait=True)
@@ -45,11 +46,12 @@ class ActionRobotNode(object):
 
         self.color = "green"
         self.laser_data = 0.5
+        self.holding = 0
 
         #green = np.uint8([[[0,255,0 ]]])
         #hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
         #print(hsv_green)
-
+        self.pipeline = keras_ocr.pipeline.Pipeline()
         
 
         rospy.sleep(1)
@@ -69,53 +71,55 @@ class ActionRobotNode(object):
 
     def image_callback(self,msg):
         
-        #print("color scan")
-
-        #if self.want != 1:
-            #print("turning")
-            #self.my_twist.angular.z = .05
-            #self.robot_movement_pub.publish(self.my_twist)
-
+        self.my_twist.angular
         image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        if self.holding == 1: 
+            images = [image]
+            prediction_groups = self.pipeline.recognize(images)
+            print("prediction", prediction_groups)
 
-        # TODO: define the upper and lower bounds for what should be considered 'green'
-
-        if self.color == "green":
-            lower_color= np.array([60, 60, 60])
-            upper_color = np.array([65, 255, 250])
-        elif self.color == "red":
-            lower_color = np.array([161, 155, 84])
-            upper_color = np.array([179, 255, 255])
-        else: # "blue"
-            lower_color = np.array([94, 80, 2])
-            upper_color = np.array([126, 255, 255])
-        mask = cv2.inRange(hsv, lower_color, upper_color)
-
-        h, w, d = image.shape
-
-        # using moments() function, the center of the yellow pixels is determined
-        M = cv2.moments(mask)
-            # if there are any yellow pixels found
-
-        ##print ("here")
-        if M['m00'] > 0:
-            # center of the pixels in the image
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-
-            #print("cx and cy:", cx, cy)
-            if self.laser_data > 3.5:
-                self.laser_data = 3.5
-            print("lzr", self.laser_data)
-            self.my_twist.linear.x = (self.laser_data - 0.20)*.1
+        if self.holding == 0:
             
-            self.my_twist.angular.z = (w/2 - cx) * 0.001 
+            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-            if (self.my_twist.angular.x) < 0.01:
-                self.move_group_gripper.go(self.close_grip, wait=True)
-                self.move_group_arm.go(self.lift_pos, wait=True)
-            self.robot_movement_pub.publish(self.my_twist)
+            # TODO: define the upper and lower bounds for what should be considered 'green'
+
+            if self.color == "green":
+                lower_color= np.array([60, 60, 60])
+                upper_color = np.array([65, 255, 250])
+            elif self.color == "red":
+                lower_color = np.array([161, 155, 84])
+                upper_color = np.array([179, 255, 255])
+            else: # "blue"
+                lower_color = np.array([94, 80, 2])
+                upper_color = np.array([126, 255, 255])
+            mask = cv2.inRange(hsv, lower_color, upper_color)
+
+            h, w, d = image.shape
+
+            # using moments() function, the center of the yellow pixels is determined
+            M = cv2.moments(mask)
+                # if there are any yellow pixels found
+
+            ##print ("here")
+            if M['m00'] > 0:
+                # center of the pixels in the image
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+
+                #print("cx and cy:", cx, cy)
+                if self.laser_data > 3.5:
+                    self.laser_data = 3.5
+                print("lzr", self.laser_data)
+                self.my_twist.linear.x = (self.laser_data - 0.20)*.085
+                
+                self.my_twist.angular.z = (w/2 - cx) * 0.001 
+
+                if (self.laser_data) < 0.24:
+                    self.move_group_gripper.go(self.close_grip, wait=True)
+                    self.move_group_arm.go(self.lift_pos, wait=True)
+                    self.holding = 1
+                self.robot_movement_pub.publish(self.my_twist)
        
    # def signal_received(self, data):
 
