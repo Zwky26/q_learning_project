09e587070f2c1092b3bee262a21c1d2f8c01422f
Zachary Wang <zlwang@haunter.cs.uchicago.edu>
2021-05-13 17:11:07 -0500

    Added comments

--- scripts/action.py
+++ scripts/action.py
@@ -18,13 +18,14 @@ This node will subscribe to robot_action and listen for commands to pick up or
 drop a dumbbell at a specific location. It will publish messages to cmd_vel
 and moveit to move turtlebot and arm according.
 '''
-
+#constants
 PI = 3.1415926535897
 right_angle = 90*2*PI/360
 angular_speed = 15*2*PI/360
 path_prefix = os.path.dirname(__file__) + "/action_states/"
 
 def next_state_calc(r,g,b):
+    ''' get the state numbers from the positions of the dumbbells'''
     return r + (16 * b) + (4 * g)
 
 class ActionRobotNode(object):
@@ -42,17 +43,16 @@ class ActionRobotNode(object):
         self.robot_movement_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
         self.move_group_arm = moveit_commander.MoveGroupCommander("arm")
         self.move_group_gripper = moveit_commander.MoveGroupCommander("gripper")
-        self.rest_pos = [0, .7, -.3, -.3]
-        self.lift_pos = [0, .3, -.8, -.3]
-        self.turn_pos = [1.5, .3, -.8, -.3]
+        self.rest_pos = [0, .7, -.3, -.3] #rest position for the arm
+        self.lift_pos = [0, .3, -.8, -.3] #lifting the dumbbell
+        self.turn_pos = [1.5, .3, -.8, -.3] #rotate the arm to unblock the camera
         self.lower_pos = [1.5, .7, -.3, -.3]
-        self.open_grip = [0.010, 0.010]
-        self.close_grip = [0.007, 0.007]
+        self.open_grip = [0.010, 0.010] 
+        self.close_grip = [0.007, 0.007] 
         self.move_group_arm.go(self.rest_pos, wait=True)
         self.move_group_gripper.go(self.open_grip, wait=True)
 
         self.laser_scan = rospy.Subscriber("/scan", LaserScan, self.object_identify)
-
         self.image_sub = rospy.Subscriber('camera/rgb/image_raw',Image, self.image_callback)
         self.my_twist = Twist(linear=Vector3(0, 0, 0),angular=Vector3(0, 0, 0))
         self.robot_movement_pub.publish(self.my_twist)
@@ -62,15 +62,13 @@ class ActionRobotNode(object):
         self.w = 0 
         self.q = []
 
-        # open file in read mode
+        # read in saved qmatrix
         with open('qmatrix.csv', 'r') as read_obj:
-            # pass the file object to reader() to get the reader object
             csv_reader = reader(read_obj)
-            # Iterate over each row in the csv using reader object
             for row in csv_reader:
-                # row variable is a list that represents a row in csv
                 self.q.append(list(map(int, row)))
-        
+                
+        #read in action matrix
         self.action_matrix = np.loadtxt(path_prefix + "action_matrix.txt")
         colors = ["red", "green", "blue"]
         self.actions = np.loadtxt(path_prefix + "actions.txt")
@@ -85,11 +83,10 @@ class ActionRobotNode(object):
         g = 0
         b = 0
         count = 0
-        #while (int(max(self.q[current_state])) > 0):
+        #while not in the final state
         while(current_state != 39):
+            #select the action corresponding to max q-value
             biggest = max(self.q[current_state])
-            #print("row" , self.q[current_state])
-            #print("biggest", biggest)
             viable = []
             for i in range(len(self.q[current_state])):
                 if int(self.q[current_state][i]) == biggest:
@@ -98,11 +95,11 @@ class ActionRobotNode(object):
                     #print(d)
                     if ((r != d) and (b != d) and (g != d)):
                         viable.append(i)
-            action = random.choice(viable)
+            action = random.choice(viable) #if a tie, randomly pick one
             act = self.actions[action]
             robot_db = act['dumbbell']
             block_id = act['block']
-            #print(robot_db, block_id)
+            #updating saved positions of robot, dumbbell
             self.to_do.append((robot_db, block_id))
             if robot_db == "red":
                 r = block_id
@@ -111,46 +108,37 @@ class ActionRobotNode(object):
             elif robot_db == "blue":
                 b = block_id
             current_state = next_state_calc(r,g,b)
-            #print("new state" , current_state)
-            #break
-            #count = count + 1
-        #print(to_do)
+            
 
         self.count = 0
         self.color, self.block_id = self.to_do[self.count]
-
-        #green = np.uint8([[[0,255,0 ]]])
-        #hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
-        #print(hsv_green)
         self.pipeline = keras_ocr.pipeline.Pipeline()
         
         rospy.sleep(1)
 
-    def pickup_dumbbell(self):
-        self.move_group_gripper.go(self.close_grip, wait=True)
-        self.move_group_arm.go(self.lift_pos, wait=True)
-
     def lower_dumbbell(self):
+        '''helper function, just lowers the dumbbell'''
         self.move_group_arm.go(self.rest_pos, wait=True)
         self.move_group_gripper.go(self.open_grip, wait=True)
 
 
     def object_identify(self,msg):
-    
+        '''saves the distance of object directly in front'''
         self.laser_data = msg.ranges[0]
 
     def image_callback(self,msg):
-        
+        ''' saves snapshot taken'''
         self.image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
         h, w, d = self.image.shape
         self.w = w
 
     def dumbel_rec(self):
-
+        ''' if not currently holding a dumbbell, go to the dumbbell specified
+        pick it up, and signal you are ready to move to a block
+        '''
         if self.holding == 0:
-            #print("here1")
+            #getting the proper color range
             hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
-            # TODO: define the upper and lower bounds for what should be considered 'green'
             if self.color == "green":
                 lower_color= np.array([60, 60, 60])
                 upper_color = np.array([65, 255, 250])
@@ -161,11 +149,9 @@ class ActionRobotNode(object):
                 lower_color = np.array([94, 80, 2])
                 upper_color = np.array([126, 255, 255])
             mask = cv2.inRange(hsv, lower_color, upper_color)
-            # using moments() function, the center of the yellow pixels is determined
             M = cv2.moments(mask)
-                # if there are any yellow pixels found
-            ##print ("here")
-            if M['m00'] > 0:
+                
+            if M['m00'] > 0: #we can see pixels of that color
                 # center of the pixels in the image
                 cx = int(M['m10']/M['m00'])
                 cy = int(M['m01']/M['m00'])
@@ -175,7 +161,7 @@ class ActionRobotNode(object):
                 print("lzr", self.laser_data)
                 self.my_twist.linear.x = max (.0075,((self.laser_data - 0.24)*.085))
                 self.my_twist.angular.z = (self.w/2 - cx) * 0.001 
-                if (self.laser_data) < 0.24:
+                if (self.laser_data) < 0.24: #close enough to grab
                     self.my_twist.linear.x = 0
                     self.robot_movement_pub.publish(self.my_twist)
                     self.move_group_gripper.go(self.close_grip, wait=True)
@@ -187,6 +173,7 @@ class ActionRobotNode(object):
                 self.robot_movement_pub.publish(self.my_twist)
 
     def move_backwards(self):
+        ''' helper function to move turtlebot backwards for set time'''
         self.my_twist.linear.x = -.1
         t0 = rospy.Time.now().to_sec()
         time_count = 0
@@ -198,6 +185,7 @@ class ActionRobotNode(object):
         self.robot_movement_pub.publish(self.my_twist)
 
     def turn_right(self):
+        ''' helper function to turn 90 degree turn'''
         current_angle = 0
         self.my_twist.angular.z = angular_speed
         t0 = rospy.Time.now().to_sec()    
@@ -207,20 +195,21 @@ class ActionRobotNode(object):
             current_angle = angular_speed * (t1-t0) 
 
     def image_rec(self):
+        ''' if we are holding a dumbbell, go to the block specified by the action
+        place the dumbbell, and signal to get the next dumbbell
+        '''
         if self.holding == 1:
-            #print("id", self.block_id)
-
+            #keras can misread certain symbols. This covers our bases
             threes = ["3","s","e","5"]
             twos = ["2"]
             ones = ["1","l", "1l"]
-
             
-            #self.robot_movement_pub.publish(self.my_twist)
+            #turn towards the blocks
             self.move_backwards()
             self.turn_right()
             
             block_count = 0
-            while block_count < 5:
+            while block_count < 5: #scan the blocks to recognize their labels
                 if self.laser_data < 3.5: 
                     self.my_twist.angular.z = 0
                     self.robot_movement_pub.publish(self.my_twist)
@@ -232,7 +221,6 @@ class ActionRobotNode(object):
                     left = 1000
                     cx_dict = {}
                     for b,a in p[0]:
-                        #print("prediction?", b )
                         cx = 0
                         for i in a:
                             if i[0] < left: 
@@ -240,10 +228,6 @@ class ActionRobotNode(object):
                                 left = i[0]
                             print(i) 
                             cx += i[0]
-                        #print("batch")
-                        #cx = cx /4
-                        #cx_dict[b] = cx
-                    #cx_final = cx_dict[id]
                     print("prediction" , id)
                     if id in ones:
                         id2 = 1
@@ -287,6 +271,7 @@ class ActionRobotNode(object):
             print("hit three blocks")
 
     def run(self):
+        ''' loops, getting a dumbbell and putting it down, until all are placed'''
         while (not rospy.is_shutdown()):
             self.dumbel_rec()
             #print("switch modes")
